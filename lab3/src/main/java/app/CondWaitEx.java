b6d17a315eaa9b23e22d6f6f755e981864c5f5a4
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-10-15 10:59:29 +0200

    wip

--- lab3/src/main/java/app/CondWaitEx.java
+++ lab3/src/main/java/app/CondWaitEx.java
@@ -24,15 +24,34 @@ abstract class Person extends Thread {
 }
 
 class Producer extends Person {
-    private Buffer _buf;
+    private final Buffer _buf;
+
+    Producer(Buffer buf) {
+        this._buf = buf;
+    }
 
 
     public void run() {
         for (int i = 0; i < 100; ++i) {
-            try {
-                TimeUnit.MILLISECONDS.wait(this.sleepTime);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+//            try {
+//                TimeUnit.MILLISECONDS.wait(this.sleepTime);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
+            synchronized (_buf) {
+                int waitCnt = 0;
+                while (_buf.isFull()) {
+                    try {
+                        System.out.println(String.format("%d. Waiting for buffer to not be full", waitCnt++));
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                _buf.put(1);
+                System.out.println(String.format("Producing -> buff size=%d", _buf.getBuffCount()));
             }
 
             _buf.put(i);
@@ -41,34 +60,92 @@ class Producer extends Person {
 }
 
 class Consumer extends Person {
-    private Buffer _buf;
+    private final Buffer _buf;
+
+    Consumer(Buffer buf) {
+        this._buf = buf;
+    }
 
     public void run() {
         for (int i = 0; i < 100; ++i) {
-            try {
-                TimeUnit.MILLISECONDS.wait(this.sleepTime);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+//            try {
+//                TimeUnit.MILLISECONDS.wait(this.sleepTime);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
 
-            System.out.println(_buf.get());
+            synchronized (_buf) {
+                int waitCnt = 0;
+
+                while (_buf.isEmpty()) {
+                    try {
+                        System.out.println(String.format("%d. Waiting for buffer to not be full", waitCnt++));
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                System.out.println(String.format("Consuming -> buff size=%d",_buf.get()));
+            }
         }
     }
 }
 
 class Buffer {
-    public void put(int i) {
-        // Implementation needed
+    private final int buffSize;
+    private int buffCount;
+
+    Buffer (int size) {
+        this.buffSize = size;
     }
 
-    public int get() {
-        // Implementation needed
-        return 0;
+    synchronized public void put(int i) {
+        if (!this.isFull()) {
+            this.buffCount += i;
+        }
+
+        this.notifyAll();
+    }
+
+    synchronized public int get() {
+        if (!this.isEmpty()) {
+            this.notifyAll();
+            return 0;
+        }
+
+        int ret = this.buffCount--;
+        this.notifyAll();
+
+        return ret;
+    }
+
+    synchronized public int getBuffCount() {
+        return this.buffCount;
+    }
+
+    public synchronized boolean isFull() {
+        return this.buffCount >= this.buffSize;
+    }
+
+    public synchronized boolean isEmpty() {
+        return this.buffCount == 0;
     }
 }
 
 public class CondWaitEx {
     public static void main(String[] args) {
-        // Implementation needed
+        int bufferSize = 5;
+        Buffer buffer = new Buffer(bufferSize);
+
+        Producer producer = new Producer(buffer);
+        Consumer consumer = new Consumer(buffer);
+
+        try {
+            producer.start();
+            consumer.start();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
