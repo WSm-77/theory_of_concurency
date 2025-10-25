4bf43c1897c6fbee40504b860ecc82ebf75c3151
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-10-25 22:45:42 +0200

    Add mean wait time measurement for philosophers

--- lab4/lab4/src/main/java/app/AbstractPhilosopher.java
+++ lab4/lab4/src/main/java/app/AbstractPhilosopher.java
@@ -1,10 +1,11 @@
 package app;
 
-import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
 
-abstract public class AbstractPhilosopher implements Runnable {
-    public static final int SLEEP_TIME = 500;
+abstract public class AbstractPhilosopher implements Callable<Double> {
+    public static final int CYCLES_COUNT = 10;
+    public static final int SLEEP_TIME = 10;
     protected final Random random = new Random();
     protected final int id;
 
@@ -26,13 +27,22 @@ abstract public class AbstractPhilosopher implements Runnable {
     }
 
     @Override
-    public void run() {
+    public Double call() {
+        Double totalWaitTime = 0.0;
+
         try {
             // start with random delay for each philosopher
             Thread.sleep(this.random.nextInt(AbstractPhilosopher.SLEEP_TIME));
 
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < AbstractPhilosopher.CYCLES_COUNT; i++) {
+                // measure waiting time
+                long startTime = System.nanoTime();
+
                 this.acquireForks();
+
+                long elapsedTime = System.nanoTime() - startTime;
+                totalWaitTime += elapsedTime;
+
                 this.eat();
                 this.releaseForks();
                 this.think();
@@ -40,6 +50,8 @@ abstract public class AbstractPhilosopher implements Runnable {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        return totalWaitTime / AbstractPhilosopher.CYCLES_COUNT;
     }
 
     abstract public void acquireForks() throws InterruptedException;
--- lab4/lab4/src/main/java/app/Main.java
+++ lab4/lab4/src/main/java/app/Main.java
@@ -2,141 +2,154 @@ package app;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-    public static void execute(int sol) {
-        switch (sol) {
-            case 1: // v1: Fork objects + Philosopher1
-            {
-                int forkCount = 5;
-                List<Fork> forks = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new Fork(i));
+    public static void execute(int sol, int forkCount) {
+        System.out.println("Sol: " + sol);
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(6)) {
+            List<Double> meanExecutionTimes;
+            List<Future<Double>> meanExecutionTimeFutures = new ArrayList<>();
+
+            switch (sol) {
+                case 1: // v1: Fork objects + Philosopher1
+                {
+                    List<Fork> forks = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new Fork(i));
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher1(i, forks));
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
                 }
 
-                List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher1(i, forks));
+                case 2: // v2: Locks + Philosopher2
+                {
+                    List<Lock> forks = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new ReentrantLock());
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher2(i, forks));
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
                 }
 
-                for (AbstractPhilosopher philosopher : philosophers) {
-                    new Thread(philosopher).start();
+                case 3: // v3: Semaphores + Philosopher3
+                {
+                    List<Semaphore> forks = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new Semaphore(1));
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher3(i, forks));
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
                 }
-                break;
+
+                case 4: // v4: tryLock + Philosopher4
+                {
+                    List<Lock> forks = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new ReentrantLock());
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher4(i, forks));
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
+                }
+
+                case 5: // v5: Mediator + Philosopher5
+                {
+                    List<Lock> forks = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new ReentrantLock());
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher5(i, forks));
+                    }
+
+                    Mediator5 mediator = new Mediator5((Philosopher5) philosophers.get(forkCount - 1));
+                    for (AbstractPhilosopher philosopher : philosophers) {
+                        ((Philosopher5)philosopher).setMediator(mediator);
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
+                }
+
+                case 6: // v6: canteen semaphore + Philosopher6
+                {
+                    List<Lock> forks = new ArrayList<>(forkCount);
+                    Semaphore canteen = new Semaphore(forkCount - 1);
+
+                    for (int i = 0; i < forkCount; i++) {
+                        forks.add(new ReentrantLock());
+                    }
+
+                    List<AbstractPhilosopher> philosophers = new ArrayList<>(forkCount);
+                    for (int i = 0; i < forkCount; i++) {
+                        philosophers.add(new Philosopher6(i, forks, canteen));
+                    }
+
+                    meanExecutionTimes = Main.getMeanExecutionTime(philosophers, executor);
+                    break;
+                }
+
+                default:
+                    System.err.println("Unknown solution number: " + sol + " (expected 1..6)");
+                    throw new RuntimeException("Unknown solution number: " + sol + " (expected 1..6)");
             }
 
-            case 2: // v2: Locks + Philosopher2
-            {
-                int forkCount = 5;
-                List<Lock> forks = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new ReentrantLock());
-                }
-
-                List<Philosopher2> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher2(i, forks));
-                }
-
-                for (Philosopher2 philosopher : philosophers) {
-                    new Thread(philosopher).start();
-                }
-                break;
-            }
-
-            case 3: // v3: Semaphores + Philosopher3
-            {
-                int forkCount = 5;
-                List<Semaphore> forks = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new Semaphore(1));
-                }
-
-                List<Philosopher3> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher3(i, forks));
-                }
-
-                for (Philosopher3 philosopher : philosophers) {
-                    new Thread(philosopher).start();
-                }
-                break;
-            }
-
-            case 4: // v4: tryLock + Philosopher4
-            {
-                int forkCount = 5;
-                List<Lock> forks = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new ReentrantLock());
-                }
-
-                List<Philosopher4> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher4(i, forks));
-                }
-
-                for (Philosopher4 philosopher : philosophers) {
-                    new Thread(philosopher).start();
-                }
-                break;
-            }
-
-            case 5: // v5: Mediator + Philosopher5
-            {
-                int forkCount = 5;
-                List<Lock> forks = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new ReentrantLock());
-                }
-
-                List<Philosopher5> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher5(i, forks));
-                }
-
-                Mediator5 mediator = new Mediator5(philosophers.get(forkCount - 1));
-                for (Philosopher5 philosopher : philosophers) {
-                    philosopher.setMediator(mediator);
-                    new Thread(philosopher).start();
-                }
-                break;
-            }
-
-            case 6: // v6: canteen semaphore + Philosopher6
-            {
-                int forkCount = 5;
-                List<Lock> forks = new ArrayList<>(forkCount);
-                Semaphore canteen = new Semaphore(forkCount - 1);
-
-                for (int i = 0; i < forkCount; i++) {
-                    forks.add(new ReentrantLock());
-                }
-
-                List<Philosopher6> philosophers = new ArrayList<>(forkCount);
-                for (int i = 0; i < forkCount; i++) {
-                    philosophers.add(new Philosopher6(i, forks, canteen));
-                }
-
-                for (Philosopher6 philosopher : philosophers) {
-                    new Thread(philosopher).start();
-                }
-                break;
-            }
-
-            default:
-                System.err.println("Unknown solution number: " + sol + " (expected 1..6)");
+            meanExecutionTimes.forEach((time) -> System.out.printf("Executing %f ms.\n", time / 1_000_000.0));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
-   public static void main(String[] args) {
+    public static List<Double> getMeanExecutionTime(List<AbstractPhilosopher> philosophers, ExecutorService executor) throws ExecutionException, InterruptedException {
+        List<Double> meanExecutionTimes = new ArrayList<>();
+        List<Future<Double>> meanExecutionTimeFutures = new ArrayList<>();
+
+        for (AbstractPhilosopher philosopher : philosophers) {
+            meanExecutionTimeFutures.add(executor.submit(philosopher));
+        }
+
+        for (Future<Double> meanExecTimeFuture : meanExecutionTimeFutures) {
+            meanExecutionTimes.add(meanExecTimeFuture.get());
+        }
+
+        return meanExecutionTimes;
+    }
+
+    public static void main(String[] args) {
         // Choose which solution to run (1..6)
         final int sol = 6; // change this value to run different variants
+        final int forkCount = 5;
 
-        Main.execute(sol);
+        Main.execute(sol, forkCount);
     }
 }
